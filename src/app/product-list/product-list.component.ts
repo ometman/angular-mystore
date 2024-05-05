21cfa8abe0308aeaf48bd7fca982ef7340ec5e66
ometman <revomet@gmail.com>
2024-05-05 05:07:54 +0000

    Add onNotify method to product list comp.ts

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -13,7 +13,11 @@ export class ProductListComponent {
   share() {
     window.alert('The product has been shared!');
   }
-}
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+ }
 
 
 /*
