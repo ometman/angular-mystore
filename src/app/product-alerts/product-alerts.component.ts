1f8baa530e89353f9cfbb9e539e2766d0d152843
ometman <revomet@gmail.com>
2024-05-05 05:03:57 +0000

    Add output and event emitter to alert comp ts

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../products'
 
 @Component({
@@ -9,5 +9,6 @@ import { Product } from '../products'
 export class ProductAlertsComponent {
 
   @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
 
 }
