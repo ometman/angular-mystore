bed0db03ea201be3c9ebbed7337b6a65cc5ebece
ometman <revomet@gmail.com>
2024-05-05 04:43:30 +0000

    Modify product alerts component .ts

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from '../products'
 
 @Component({
   selector: 'app-product-alerts',
-  standalone: true,
-  imports: [],
   templateUrl: './product-alerts.component.html',
   styleUrl: './product-alerts.component.css'
 })
 export class ProductAlertsComponent {
+
+  @Input() product: Product | undefined;
 
 }
