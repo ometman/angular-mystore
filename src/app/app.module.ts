5779ce66ea1d2fb249ab0f692c511e0ac6f96c3c
ometman <revomet@gmail.com>
2024-05-05 02:55:46 +0000

    Modify app.module, product-list, product-details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   imports: [
@@ -13,6 +14,7 @@ import { ProductListComponent } from './product-list/product-list.component';
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent }
     ])
   ],
   declarations: [
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Product, products } from '../products'
 
 @Component({
   selector: 'app-product-details',
@@ -7,6 +10,20 @@ import { Component } from '@angular/core';
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+
+  product:  Product | undefined;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    // get product from current route
+    const routeParams = this.route.snapshot.paramMap
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    //find product that corresponds with the proudct Id in route
+    this.product = products.find(product => product.id === productIdFromRoute);
+    
+  }
 
 }
