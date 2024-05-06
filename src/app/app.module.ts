2cdcccf163d5067895316723300a24f428958e59
ometman <revomet@gmail.com>
2024-05-06 10:33:39 +0000

    Create shipping component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ProductAlertsComponent } from './product-alerts/product-alerts.componen
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component'
 import { HttpClientModule } from '@angular/common/http';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   imports: [
@@ -20,6 +21,7 @@ import { HttpClientModule } from '@angular/common/http';
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
       { path: 'cart', component: CartComponent },
+      { path: 'shipping', component: ShippingComponent}
     ])
   ],
   declarations: [
@@ -29,6 +31,7 @@ import { HttpClientModule } from '@angular/common/http';
     ProductAlertsComponent,
     ProductDetailsComponent,
     CartComponent,
+    ShippingComponent,
   ],
   providers: [], // Optionally, include providers array if needed
   bootstrap: [AppComponent]
--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-shipping',
-  standalone: true,
-  imports: [],
   templateUrl: './shipping.component.html',
   styleUrl: './shipping.component.css'
 })
