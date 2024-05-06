bf38e8859ccba6ba12056e16661b6ed81e73ac89
ometman <revomet@gmail.com>
2024-05-06 10:10:39 +0000

    Configure CartService for shipping prices

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -24,4 +24,10 @@ export class CartService {
     this.items = [];
     return this.items;
   }
+
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number }[]>
+    ('/assets/shipping.json')
+
+  }
 }
