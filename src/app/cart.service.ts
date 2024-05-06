791c76482219bcb9781ef0851ae786d9d9d438ef
ometman <revomet@gmail.com>
2024-05-06 05:46:39 +0000

    Define methods to add, return, clear cart items

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -8,4 +8,17 @@ export class CartService {
 
   constructor() { }
   items: Product[] = []
+
+  addToCart (product: Product) {
+    this.items.push(product);
+  }
+
+  getItems () {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
 }
