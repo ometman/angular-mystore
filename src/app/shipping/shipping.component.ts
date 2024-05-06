75544976fdfcf097b4e4697cd8c4c0573c295d66
ometman <revomet@gmail.com>
2024-05-06 10:12:25 +0000

    generate the shipping.component.ts

---
+++ src/app/shipping/shipping.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shipping',
+  standalone: true,
+  imports: [],
+  templateUrl: './shipping.component.html',
+  styleUrl: './shipping.component.css'
+})
+export class ShippingComponent {
+
+}
