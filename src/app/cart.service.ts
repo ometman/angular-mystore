dd6d6062e8d0895992d84266e1d00aa8a2fee217
ometman <revomet@gmail.com>
2024-05-06 05:41:06 +0000

    Import Product interface  into cart.service.ts

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Product } from './products'
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,5 @@ import { Injectable } from '@angular/core';
 export class CartService {
 
   constructor() { }
+  items: Product[] = []
 }
