197606dad258aa80d8c75f2cf0e86a0fbc1235ed
ometman <revomet@gmail.com>
2024-05-06 10:45:11 +0000

    Configuring ShippingComponent to use CartService

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-shipping',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './shipping.component.css'
 })
 export class ShippingComponent {
+
+  constructor(
+    private cartService: CartService
+  ) { }
+
+  shippingCosts!: Observable<{ type: string, price: number }[]>
+
+  ngOnInit(): void {
+
+    this.shippingCosts = this.cartService.getShippingPrices();
+
+  }
 
 }
